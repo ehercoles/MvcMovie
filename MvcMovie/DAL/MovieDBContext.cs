6572c1e68637cd75cc4a264e58828a93193c815a
Erasmus Ercoles <ehercoles@gmail.com>
2018-02-25 16:00:21 -0300

    https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/getting-started

---
+++ MvcMovie/DAL/MovieDBContext.cs
@@ -0,0 +1,10 @@
+﻿using MvcMovie.Models;
+using System.Data.Entity;
+
+namespace MvcMovie.DAL
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
